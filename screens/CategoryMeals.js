273c4b1d98621abf11e4d6edde1d46be49e25da3
Drinnn <pedro_lontro@hotmail.com>
2021-01-26 16:27:36 -0300

    feat: add navigation to meals categories

--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
+import colors from "../constants/colors";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMeals = (props) => {
+  const categoryId = props.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find(
+    (category) => category.id === categoryId
+  );
+
   return (
     <View style={styles.container}>
       <Text>Category Meals</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meal Details"
         onPress={() => {
@@ -13,6 +22,21 @@ const CategoryMeals = (props) => {
       />
     </View>
   );
+};
+
+CategoryMeals.navigationOptions = (navigationData) => {
+  const categoryId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(
+    (category) => category.id === categoryId
+  );
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? colors.primary : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : colors.primary,
+  };
 };
 
 const styles = StyleSheet.create({
