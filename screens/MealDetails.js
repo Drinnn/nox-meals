2e5ff21d31de285747ff0f78376c2dbcfd69dbba
Drinnn <pedro_lontro@hotmail.com>
2021-01-27 11:38:46 -0300

    feat: add button on meal details header

--- screens/MealDetails.js
+++ screens/MealDetails.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
 import { MEALS } from "../data/dummy-data";
 
 const MealDetails = (props) => {
@@ -20,6 +22,15 @@ MealDetails.navigationOptions = (navigationData) => {
 
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => console.log("marked as favorite")}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
